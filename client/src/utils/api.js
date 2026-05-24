abb80c51c1d86bdb30f47504738b206bb045732f
lucioarchimede <larchimede@udesa.edu.ar>
2026-05-23 21:56:12 -0300

    Fix: Export api instance with hardcoded backend URL

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,3 +1,10 @@
+import axios from 'axios'
+
 const BASE = 'https://dashboard-sxkz.onrender.com'
 
 export const API_URL = BASE
+
+export const api = axios.create({
+  baseURL: BASE,
+  withCredentials: true
+})
